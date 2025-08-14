65873826325b1a2929345e203d3be1b12e02a7d5
Túlio Sousa <contato@tuliosousa.pro>
2025-08-14 11:29:58 -0300

    Update scroll-area.tsx

--- package/components/ui/scroll-area.tsx
+++ package/components/ui/scroll-area.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ScrollAreaPrimitive from '@radix-ui/react-scroll-area';
 import { cn } from './utils';
 
-export const ScrollArea = ({ className, children, ...props }: React.HTMLAttributes<HTMLDivElement>) => {
+export const ScrollArea: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({ className, children, ...props }) => {
   return (
     <ScrollAreaPrimitive.Root className={cn('h-48 w-full overflow-hidden', className)} {...props}>
       <ScrollAreaPrimitive.Viewport className="w-full h-full">{children}</ScrollAreaPrimitive.Viewport>
